de575a76bebf54675d14e2ea2cb46fb9b9f5c015
andreathepa <andreadepa32@gmail.com>
2023-06-21 17:36:26 +0200

    blocks structure done

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,16 +1,139 @@
 <script>
 export default {
-    
+    data(){
+        return{
+            icons: 
+            [
+                {
+                    title: 'DIGITAL COMICS',
+                    icona: "../assets/img/buy-comics-digital-comics.png",
+                    active: false,
+                },
+                {
+                    title: 'DC MERCHANDISE',
+                    icona: "../assets/img/buy-comics-merchandise.png",
+                    active: false,
+                },
+                {
+                    title: 'SUBSCRIPTION',
+                    icona: "../assets/img/buy-subscriptions.png",
+                    active: false,
+                },
+                {
+                    title: 'COMIC SHOP LOCATOR',
+                    icona: "../assets/img/buy-shop-locator.png",
+                    active: false,
+                },
+                {
+                    title: 'DC POWER VISA',
+                    icona: "../assets/img/buy-dc-power-visa.png",
+                    active: false,
+                },
+            ]
+        };
+    }
+
 }
 </script>
 
 <template lang="">
     <div>
-       <h1>Footer</h1>
-       <h1>Signup now</h1> 
+       <div class="footer_blue flex justify-content-center align-items-center">
+        <ul>
+            <li v-for="(icon, index) in icons" :key="index">
+                <a>
+                    <div>
+                        {{ icon.title }}
+
+                    </div>
+                    <div>
+                        {{icon.icona}}
+                    </div>
+                </a>
+            </li>
+        </ul>
+       </div>
+       <div class="footer_bg">
+            <img src="../assets/img/footer-bg.jpg" class="bg_img" alt="">
+            <img src="../assets/img/dc-logo-bg.png" class="bg_logo" alt="">
+       </div>
+       <div class="container_sign_up">
+           <div class="sign_up flex justify-content-between align-items-center">
+                <div class="">
+                    <button class="">
+                        SIGN-UP NOW!
+                    </button>
+                </div>
+                <div>
+                    <ul>
+                        <li>FOLLOW US</li>
+                        <li>ciao</li>
+                        <li>ciao</li>
+                        <li>ciao</li>
+                        <li>ciao</li>
+                    </ul>
+                </div>
+           </div> 
+
+       </div>
     </div>
 </template>
 
-<style lang="">
+<style lang="scss" scoped>
+
+    ul li{
+        display: inline-block;
+        padding: 30px 20px;
+    }
+    .footer_blue{
+        height: 150px;
+        background-color: rgb(66, 100, 253);
+        margin: 0 auto;
+    }
+
+    .footer_bg{
+        position: relative;
+        overflow: hidden;
+        width: 100%;
+        height: 50vh;
+
+        .bg_img{
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 140%;
+            object-fit: cover;
+            
+        }
+
+        .bg_logo{
+            height: 604px;
+            position: absolute;
+            top: -46px;
+            right: 287px;
+        }
+        
+    }
+
+    .container_sign_up{
+        background-color: rgb(37, 37, 37);
+
+        .sign_up{
+                height: 100px;
+                margin: 0 auto;
+                max-width: 1320px;
     
+                button{
+                    padding: 15px;
+                    border: 1px solid blue;
+                    cursor: pointer;
+                    background-color: rgb(37, 37, 37) ;
+                    color: white;
+                }
+    
+                
+            }
+
+    }
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,11 +6,34 @@ export default {
 
 <template lang="">
     <div>
-        <h1>Content black goes here</h1>
-        <h1>Content blue goes here</h1>
+        <div class="jumbotron">
+            <img src="../assets/img/jumbotron.jpg" alt="">
+        </div>
+        <div class="prova">Content blue goes here</div>
     </div>
 </template>
 
-<style lang="">
+<style lang="scss" scoped>
+    .jumbotron{
+        position: relative;
+        overflow: hidden;
+        width: 100%;
+        height: 50vh;
+
+        img{
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 200%;
+            object-fit: cover;
+            
+        }
+    }   
+    .prova{
+        height: 1000px;
+        background-color: black;
+    }
+
     
 </style>
